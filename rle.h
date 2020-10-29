34c0080a49f8c14b408d77480af239ea5a154ebc
dgrat <dgdanielf@gmail.com>
2020-10-29 19:02:50 +0100

    run length encoding
    Signed-off-by: dgrat <dgdanielf@gmail.com>

---
+++ rle.h
@@ -0,0 +1,141 @@
+#pragma once
+
+#include <cassert>
+#include <list>
+#include <vector>
+
+
+template<class base_t>
+struct rle_chunk {
+    size_t _repetitions;
+    base_t _value;
+
+    operator base_t() const {
+        return _value;
+    }
+    operator base_t&() {
+        return _value;
+    }
+};
+
+template<class base_t>
+struct rle_meta {
+    // chunk: how many times value exists ..
+    std::list<rle_chunk<base_t>> _chunks = {};
+    // original storage container size
+    size_t _uncompressed_size = 0;
+};
+
+template<class base_t>
+class rle {
+    rle_meta<base_t> _rle;
+    using chunk_t = rle_chunk<base_t>;
+
+protected:
+    void init(const base_t &val) {
+        clear();
+        _rle._uncompressed_size = 1;
+        _rle._chunks = {{ 1, val }};
+    }
+
+    void add(const base_t &val) {
+        if(_rle._chunks.empty() || _rle._uncompressed_size < 1) {
+            init(val);
+            return;
+        }
+
+        if(val == _rle._chunks.rbegin()->_value) {
+            _rle._chunks.rbegin()->_repetitions++;
+        }
+        else {
+            _rle._chunks.push_back({ 1, val });
+        }
+        _rle._uncompressed_size++;
+    }
+
+public:
+    const auto &data() const {
+        return _rle;
+    }
+
+    auto &data() {
+        return _rle;
+    }
+
+    void clear() {
+        _rle._uncompressed_size = 0;
+        _rle._chunks.clear();
+    }
+
+    //! add value to rle
+    void operator << (const base_t &val) {
+        add(val);
+    }
+
+    const auto operator [] (const size_t id) const {
+        return get(id);
+    }
+
+    //! returns an iterator to an element in the rle array at a given position
+    //! iterator::end() if nothing was found
+    auto get(const size_t id) const {
+        size_t pos = 0;
+        using cont_t = std::list<rle_chunk<base_t>>;
+        typename cont_t::const_iterator it = _rle._chunks.begin();
+        for (const auto &chunk : _rle._chunks) {
+            pos += chunk._repetitions;
+            if(id >= pos) {
+                it++;
+                continue;
+            }
+            return it;
+        } 
+        return _rle._chunks.end();
+    }
+
+    //! change the value of a position in the rle data
+    //! search overhead
+    void set(const size_t id, const base_t &val) {
+        size_t chunk_end = 0;
+        auto it = _rle._chunks.begin();
+        for (auto &chunk : _rle._chunks) {
+            chunk_end += chunk._repetitions;
+            it++;
+            if(id >= chunk_end) {
+                continue;
+            }
+
+            const size_t back_reps = (chunk_end-id) - 1;
+            const size_t frnt_reps = (chunk._repetitions - (chunk_end-id));
+            const size_t mid_reps = 1;
+
+            // change or delete current chunk
+            chunk._repetitions = frnt_reps;
+            it = _rle._chunks.insert(it, {back_reps, chunk._value});
+            _rle._chunks.insert(it, {mid_reps, val});
+            break;
+        } 
+    }
+
+    //! rle -> array
+    std::vector<base_t> decode() const {
+        std::vector<base_t> buf;
+        for (const auto &iter : _rle._chunks) {
+            for(int i = 0; i < iter._repetitions; i++) {
+                buf.push_back(iter._value);
+            }
+        }
+        return buf;
+    }
+
+    //! array -> rle
+    void encode(const std::vector<base_t> &buf) {
+        for (size_t x = 0; x < buf.size(); x++) {
+            add(buf.at(x));
+        }
+    }
+
+    // ctor section
+    rle() = default;
+    rle(const std::vector<base_t> &buf) { encode(buf); }
+};
---
+++ test_rle.cpp
@@ -0,0 +1,188 @@
+#include "rle.h"
+#include "rle_io.h"
+
+#include <iostream>
+#include <gtest/gtest.h> // googletest header file
+
+
+std::vector<int> ref1 = { 0,1,2,3,4 };
+std::vector<int> ref2 = { 0,0 };
+std::vector<int> ref3 = { 0,0,1,1,1,2,2,2,2,3,3,3,3,3,4,4,4,4,4,4 };
+std::vector<int> ref4 = { };
+std::vector<int> ref5 = { 9 };
+std::vector<int> ref6 = { 0,0,1,1,2,2,3,3,4,4,5,5 };
+
+
+std::vector<int> test_io(std::vector<int> v) {
+    rle<int> rle(v);
+    rle_io<int> out_rle(rle);
+    out_rle.to_file("tmp.rle");
+
+    rle_io<int> in_rle;
+    in_rle.from_file("tmp.rle");
+    return in_rle.rle().decode();
+}
+
+std::vector<int> enc1(std::vector<int> v) {
+    return rle<int>(v).decode();
+}
+
+std::vector<int> enc2(std::vector<int> arr) {
+    rle<int> a;
+    for(auto val : arr) {
+        a << val;
+    }
+    return a.decode();
+}
+
+std::vector<int> enc3(std::vector<int> arr) {
+    rle<int> a(arr);
+    
+    std::vector<int> b;
+    for(int i = 0; i < arr.size(); i++) {
+        int v = *a[i];
+        b.push_back(v);
+    }
+    return b;
+}
+
+std::vector<int> enc4(std::vector<int> arr) {
+    rle<int> a(arr);
+    a.set(0, 9);
+    auto n =  a.decode();
+    for(auto v : n)
+        std::cerr << v << std::endl;
+
+    std::cerr << "new 1" << std::endl;
+    a = rle<int>(arr);
+    a.set(1, 9);
+    n =  a.decode();
+    for(auto v : n)
+        std::cerr << v << std::endl;
+
+    std::cerr << "new 0" << std::endl;
+    a = rle<int>(arr);
+    a.set(2, 9);
+    n =  a.decode();
+    for(auto v : n)
+        std::cerr << v << std::endl;
+
+    std::cerr << "new 3" << std::endl;
+    a = rle<int>(arr);
+    a.set(3, 9);
+    n =  a.decode();
+    for(auto v : n)
+        std::cerr << v << std::endl;
+
+    std::cerr << "new 4" << std::endl;
+    a = rle<int>(arr);
+    a.set(4, 9);
+    n =  a.decode();
+    for(auto v : n)
+        std::cerr << v << std::endl;
+
+    std::cerr << "new 5" << std::endl;
+    a = rle<int>(arr);
+    a.set(5, 9);
+    n =  a.decode();
+    for(auto v : n)
+        std::cerr << v << std::endl;
+
+    std::cerr << "new 6" << std::endl;
+    a = rle<int>(arr);
+    a.set(6, 9);
+    n =  a.decode();
+    for(auto v : n)
+        std::cerr << v << std::endl;
+
+    std::cerr << "new 11" << std::endl;
+    a = rle<int>(arr);
+    a.set(11, 9);
+    n =  a.decode();
+    for(auto v : n)
+        std::cerr << v << std::endl;
+
+    std::cerr << "new 12" << std::endl;
+    a = rle<int>(arr);
+    a.set(12, 9);
+    n =  a.decode();
+    for(auto v : n)
+        std::cerr << v << std::endl;
+    return n;
+}
+
+auto test1 = enc1(ref1);
+auto test2 = enc1(ref2);
+auto test3 = enc1(ref3);
+auto test4 = enc1(ref4);
+auto test5 = enc1(ref5);
+auto test6 = enc2(ref3);
+auto test7 = enc3(ref3);
+auto test8 = test_io(ref6);
+
+
+TEST(RLETest, ArrayComparison1) {
+    ASSERT_EQ(ref1.size(), test1.size()) << "Vectors x and y are of unequal length";
+
+    for (int i = 0; i < ref1.size(); ++i) {
+        EXPECT_EQ(ref1[i], test1[i]) << "Vectors x and y differ at index " << i;
+    }
+}
+
+TEST(RLETest, ArrayComparison2) {
+    ASSERT_EQ(ref2.size(), test2.size()) << "Vectors x and y are of unequal length";
+
+    for (int i = 0; i < ref2.size(); ++i) {
+        EXPECT_EQ(ref2[i], test2[i]) << "Vectors x and y differ at index " << i;
+    }
+}
+
+TEST(RLETest, ArrayComparison3) {
+    ASSERT_EQ(ref3.size(), test3.size()) << "Vectors x and y are of unequal length";
+
+    for (int i = 0; i < ref3.size(); ++i) {
+        EXPECT_EQ(ref3[i], test3[i]) << "Vectors x and y differ at index " << i;
+    }
+}
+
+TEST(RLETest, ArrayComparison4) {
+    ASSERT_EQ(ref4.size(), test4.size()) << "Vectors x and y are of unequal length";
+
+    for (int i = 0; i < ref4.size(); ++i) {
+        EXPECT_EQ(ref4[i], test4[i]) << "Vectors x and y differ at index " << i;
+    }
+}
+
+TEST(RLETest, ArrayComparison5) {
+    ASSERT_EQ(ref5.size(), test5.size()) << "Vectors x and y are of unequal length";
+
+    for (int i = 0; i < ref5.size(); ++i) {
+        EXPECT_EQ(ref5[i], test5[i]) << "Vectors x and y differ at index " << i;
+    }
+}
+
+
+TEST(RLETest, ArrayComparison6) {
+    ASSERT_EQ(ref3.size(), test6.size()) << "Vectors x and y are of unequal length";
+
+    for (int i = 0; i < ref3.size(); ++i) {
+        EXPECT_EQ(ref3[i], test6[i]) << "Vectors x and y differ at index " << i;
+    }
+}
+
+TEST(RLETest, ArrayComparison7) {
+    ASSERT_EQ(ref3.size(), test7.size()) << "Vectors x and y are of unequal length";
+
+    for (int i = 0; i < ref3.size(); ++i) {
+        EXPECT_EQ(ref3[i], test7[i]) << "Vectors x and y differ at index " << i;
+    }
+}
+
+
+TEST(RLETest, ArrayComparison8) {
+    ASSERT_EQ(ref6.size(), test8.size()) << "Vectors x and y are of unequal length";
+
+    for (int i = 0; i < ref6.size(); ++i) {
+        EXPECT_EQ(ref6[i], test8[i]) << "Vectors x and y differ at index " << i;
+    }
+}
